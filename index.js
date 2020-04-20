a3c7d28f4912e64855dedc8514351dc669038c12
Mahendran <mahendrananbarasan@gmail.com>
2020-04-20 22:51:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const { TransactionProcessor } = require('sawtooth-sdk/processor')                                
+const SimpleStoreHandler = require('./handler')                                                   
+const transactionProcessor = new TransactionProcessor("tcp://54.165.75.115:4004")                                                                                                                                                              
+transactionProcessor.addHandler(new SimpleStoreHandler())                                                              
+transactionProcessor.start()                                                                                                                                                                                                                    
+console.log(`Starting bank transaction processor`)                                                                      
+console.log(`Connecting to Sawtooth validator at tcp://localhost:4004`)
